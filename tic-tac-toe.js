45b94e8ac7e3bcd8dbee2f22b46091f16cf0f874
ArtanaFio <125154911+ArtanaFio@users.noreply.github.com>
2024-07-11 17:03:55 -0400

    Write method to insert symbols into gameboard
    Updated the gameboardModule IIFE with method function insertSymbols to
    only insert a symbol into an empty space of the gameboard using if
    statements and the splice array method.

--- tic-tac-toe.js
+++ tic-tac-toe.js
@@ -2,36 +2,52 @@
 // Use IIFE for single instance: gameboard, displayController
 
 // IIFE to create the gameboard object and methods to interact with it
-const gameboardModule = (function () {
+const gameboardModule = (function() {
     const gameboard = [
         ["", "", ""],
-        ["", "", "o"],
+        ["", "", ""],
         ["", "", ""]
     ];
 
     function getGameboard() {
         return gameboard;
     }
-
+    
     // method to insert player symbols into the gameboard
     function insertSymbols(row, column, symbol) {
-        // define gameboard spaces using rows and columns 
-        // based on the array's indices: array[row][column]
+        // define gameboard spaces using rows and columns based on the array's indices: array[row][column]
+        if (row >= 0 && row < gameboard.length) {
+            if (column >= 0 && column < gameboard[row].length) {
+                if (gameboard[row][column] === "") {
+                    // to insert the symbol, use splice()
+                    gameboard[row].splice(column, 1, symbol);
+                } else {
+                    console.log(`row ${row}, column ${column} is already filled. Choose another space`);
+                }
+            }
+        }
     }
-
-    // turns the function getGameboard() into an object, 
+    
+    // turns the method getGameboard() into an object, 
     // which the user can access outside the IIFE by calling the method
     // using gameboardModule.getGameboard()
     // this ultimately allows access to the current state of the gameboard
     return {
-        getGameboard: getGameboard
+        getGameboard: getGameboard,
+        insertSymbols: insertSymbols
     };
 })();
-
+// method call: IIFEobject.methodOfObject() to show the gameboard in the console
+let grid = gameboardModule.getGameboard();
 console.log("Gameboard array:");
+console.log(grid);
+gameboardModule.insertSymbols(1, 1, 'X');
+console.log("Gameboard array after inserting 'X' in the middle:");
+console.log(grid);
 
-// method call: IIFEobject.methodOfObject()
-console.log(gameboardModule.getGameboard());
+
+
+
 
 // Factory function to create player objects
 const createPlayer = (name, symbol) => {
